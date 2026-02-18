621e341928c6f48c1df1cc3b9a11464dde4748bb
milkywayn <milkyway09714@gmail.com>
2026-02-18 14:54:59 +0900

    コメント

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.post("/notify", async (req, res) => {
 
     // Embed 作成
     const embed = new EmbedBuilder()
-  .setTitle("<:lw:1473557143993978914> タイマー終了！<:lw:1473557143993978914>")
+  .setTitle("<:lw:1473557143993978914> じかんだけど～？<:lw:1473557143993978914>")
   .setDescription("@here \n**<:lw:1473557143993978914>設定した時間になりました！**<:lw:1473557143993978914>")
   .setColor(0x00ff00)
   .setTimestamp(new Date())
